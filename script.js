b4f3af6941199a8487a7f075144a3fa933f117d2
YEEET64 <84526443+YEEET64@users.noreply.github.com>
2026-09-19 17:51:29 +0100

    erste Struktur

---
+++ script.js
@@ -0,0 +1,7 @@
+const startButton = document.getElementById("startButton");
+
+startButton.addEventListener("click", function() {
+    // Hier kommt später die Spiellogik hin
+
+    console.log("Spiel gestartet!");
+});
